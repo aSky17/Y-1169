3aa248e48ba38a5b745d1feb8330e029e17600b1
Akash <akashmishra.am.1710@gmail.com>
2025-06-04 02:26:58 +0530

    added float to int and int to float operations for FPU unit

---
+++ rtl/fp32_core/fp_ftoi.sv
@@ -0,0 +1,136 @@
+`ifndef FP_FTOI_SV
+`define FP_FTOI_SV
+
+module fp_ftoi #(
+    import gpu_parameters::*
+) (
+    input logic clk,
+    input logic rst,
+    input logic [DATA_WIDTH-1:0] float_in,
+    output logic [DATA_WIDTH-1:0] int_result, //resulting signed integer
+    output logic result_valid
+);
+
+    //asserting the DATA_WIDTH is 32-bit for singls precision
+    initial begin 
+        if (DATA_WIDTH != 32) begin 
+            $fatal(1, "ERROR: DATA_WIDTH must be 32 bits");
+        end
+    end
+
+    localparam EXP_WIDTH = 8;
+    localparam MANT_WIDTH = 23;
+    localparam BIAS = 127;
+
+    //Pipelining Stage-1: Field extraction and special case detection
+    logic sign_s1;
+    logic [EXP_WIDTH-1:0] exp_s1;
+    logic [MANT_WIDHT-1:0] mant_s1;
+
+    logic is_zero_s1, is_inf_s1, is_nan_s1;
+    logic stage1_valid;
+
+    always_ff begin 
+        if (rst) begin 
+            stage1_valid <= 1'b0;
+            sign_s1 <= 1'b0;
+            exp_s1 <= 0;
+            mant_s1 <= 0;
+            is_zero_s1 <= 1'b0;
+            is_inf_s1 <= 1'b0;
+            is_nan_s1 <= 1'b0;
+        end else begin 
+            sign_s1 <= float_in[DATA_WIDTH-1];
+            exp_s1 <= float_in[DATA_WIDTH-2:MANT_WIDTH];
+            mant_s1 <= float_in[MANT_WIDTH-1:0];
+
+            is_zero_s1 <= (exp_s1 == 0) && (mant_S1 == 0);
+            is_inf_s1 <= (exp_s1 == {EXP_WIDTH{1'b1}}) && (mant_s1 == 0);
+            is_nan_s1 <= (exp_s1 == {EXP_WIDTH{1'b1}}) && (mant_s1 != 0);
+            stage1_valid <= 1'b1;
+        end
+    end
+
+
+    //Pipelining Stage-2: Denormalization, truncation and saturation
+    logic [DATA_WIDTH-1:0] int_result_s2;
+    logic result_valid_s2;
+
+    logic [EXP_WIDTH+MANT_WIDTH:0] denormalized_mantissa; //space for hidden bit and potential shifts
+    logic [EXP_WIDTH:0] biased_exponent;
+    logic [7:0] shift_amount; //MAX shift is 127 (for exponent 1) or 150 (for denormals)
+
+    always_ff @(posedge clk or posedge rst) begin 
+        if (rst) begin 
+            result_valid_s2 <= 1'b0;
+            int_result_s2 <= 0;
+        end else if (stage1_valid) begin 
+            if (is_nan_s1) begin
+                //NaN:indeterminate integer value, representing by 0
+                int_result_s2 <= 0;
+            end else if (is_inf_s1) begin 
+                //infinity: saturating to max/min integer
+                if (sign_s1) begin 
+                    //saturate to min
+                    int_result_s2 <= {1'b1, {DATA_WIDTH-1{1'b0}}};
+                end else begin 
+                    // Maximum signed integer (2^31 - 1)
+                    int_result_s2 <= {1'b0, {DATA_WIDTH-2{1'b1}}, 1'b1};
+                end
+            end else if (is_zero_s1) begin 
+                int_result_s2 <= 0;
+            end else begin 
+                //denormal processing
+                biased_exponent = exp_s1;
+                denormalized_mantissa = (exp_s1 == 0) ? {1'b0, mant_s1} : {1'b1, mant_s1}; // adding hidden bit for normalized //if exponent is zero then it denormalized mantissa
+            
+                //actual exponent
+                integer actual_exponent = biased_exponent - BIAS;
+
+                if (actual_exponent < 0) begin 
+                    //i.e., FP value is less than 1.0 like 0.5, 0.23 etc
+                    int_result_s2 <= 0;
+                end else if (actual_exponent >= DATA_WIDTH-1) begin 
+                    //value is too large to fit in 32-bit signed integer
+                    //therefore saturating to max/min integer based on sign
+                    if (sign_s1) begin 
+                        //saturating to min
+                        int_result_s2 <= {1'b1, {DATA_WIDTH-1{1'b0}}}; 
+                    end else begin
+                        //maximum signed integer 
+                        int_result_s2 <= {1'b0, {DATA_WIDTH-2{1'b1}}, 1'b1}; 
+                    end
+                end else begin 
+                    //shift to align the integer part
+                    //shift amount is actual_exponent
+                    shift_amount = actual_exponent;
+                    /*
+                    * The integer part is the bits from the denormalized mantissa shifted left
+                    * extract the integer part (before the implied decimal point)
+                    * the hidden bit is at position MANT_WIDTH (23) if normalized
+                    * so to get the integer part, we need to shift right by (MANT_WIDTH - actual_exponent
+                    */
+                    if (shift_amount >= MANT_WIDTH) begin // Integer part is fully within the 24 bits
+                        // if E = 30, we need to shift denormalized_mantissa by 30 - 24 = 6 to restore the correct scale.
+                        int_result_s2 =  denormalized_mantissa << (shift_amount - MANT_WIDTH);
+                    end else begin
+                        //EG: 1.101 × 2^1 = 11.01 (binary) = 3.25
+                        int_result_s2 = denormalized_mantissa >> (MANT_WIDTH - shift_amount);
+                    end
+
+                    //apply sign
+                    if (sign_s1)
+                        int_result_s2 = -int_result_s2; //2's complement for negative numbers
+                end
+            end
+            result_valid_s2 <= 1'b1;
+        end else begin 
+            result_valid_s2 <= 1'b0;
+        end
+    end
+    
+    assign int_result = int_result_s2;
+    assign result_valid = result_valid_s2;
+endmodule
+
+`endif
---
+++ rtl/fp32_core/fp_itof.sv
@@ -0,0 +1,115 @@
+`ifndef FP_ITOF_SV
+`define FP_ITOF_SV
+
+module fp_itof #(
+    import gpu_parameters::*
+) (
+    input logic clk,
+    input logic rst,
+    input logic [DATA_WIDTH-1:0] int_in,
+    output logic [DATA_WIDTH-1:0] float_result,
+    output logic result_valid
+);
+
+    initial begin 
+        if (DATA_WIDTH != 32) begin 
+            $fatal(1,"ERROR: DATA_WIDTH must be 32 bits");
+        end
+    end
+    
+    localparam EXP_WIDTH = 8;
+    localparam MANT_WIDTH = 23;
+    localparam BIAS = 127;
+
+    //Pipelining Stage-1: Determining sign, absolute value, and leading bit position
+    logic sign_s1;
+    logic [DATA_WIDTH-1:0] abs_int_s1;
+    logic [5:0] leading_bit_pos_s1; //max position is 31 for 32-bit int
+    logic stage1_valid;
+
+    //function to find the position of most significant 1 bit
+    function automatic [5:0] find_leading_bit_position;
+        input [DATA_WIDTH-1:0] in;
+        integer i;
+        begin
+            find_leading_bit_position = 0;
+            for (i = DATA_WIDTH-1; i>=0; i--) begin 
+                if (in[i]) begin 
+                    find_leading_bit_position = i;
+                    break;
+                end
+            end
+        end
+    endfunction
+
+    always_ff @(posedge clk or posedge rst) begin 
+        if (rst) begin 
+            stage1_valid <= 0;
+            sign_s1 <= 1'b0;
+            abs_int_s1 <= 0;
+            leading_bit_pos_s1 <= 0; 
+        end else begin 
+            sign_s1 <= int_in[DATA_WIDTH-1]; //MSB is signed bit
+            abs_int_s1 = (sign_s1) ? -int_in : int_in; //get absolute value
+            leading_bit_pos_s1 = find_leading_bit_position(abs_int_s1);
+            stage1_valid <= 1'b1;
+        end
+    end
+
+    //Pipelining Stage-2: Calculate exponent, extract mantissa, and pack result
+    logic [DATA_WIDTH-1:0] float_result_s2;
+    logic result_valid_s2;
+
+    logic [EXP_WIDTH-1:0] exp_calc;
+    logic [MANT_WIDTH-1:0] mant_calc;
+    logic is_zero_s2; //to handle int_in == 0 case
+
+    always_ff @(posedge clk or posedge rst) begin 
+        if (rst) begin 
+            result_valid_s2 <= 1'b0;
+            float_result_s2 <= 0;
+        end else if (stage1_valid) begin 
+            is_zero_s2 = (abs_int_s1 == 0);
+
+            if (is_zero_s2) begin 
+                float_result_s2 <= {sign_s1, {DATA_WIDTH-1{1'b0}}}; //signed zero
+            end else begin 
+                /*
+                * Normalized number
+                * Exponent = position of MSB + BIAS
+                * the leading_bit_pos_s1 is 0-indexed
+                * if the leading_bit_pos_s1 is 23 (2^23), the exponent is 23-23 + BIAS = BIAS
+                * if the leading_bit_pos_s1 is 0 (2^0), the exponent is (0-23) + BIAS 
+                */
+                exp_calc = (leading_bit_pos_s1 - MANT_WIDTH) + BIAS;
+
+                /* Mantissa: 23 bits after the hidden bit
+                * The hidden bit is at leading_bit_pos_s1, we need the next 23 bits
+                * shift the absolute integer left to align the hidden bit at position 23
+                * and then take the next 23 bits as the fractional part
+                */
+                if (leading_bit_pos_s1 >= MANT_WIDTH) begin 
+                    mant_calc = abs_int_s1[(leading_bit_pos_s1-1) -: MANT_WIDTH];
+                end else begin 
+                    //if the number of bits is less than MANT_WIDTH eg 5, pad with zeros
+                    mant_calc = {abs_int_s1[leading_bit_pos_s1-1:0], {MANT_WIDTH - leading_bit_pos_s1 {1'b0}}};
+                end
+
+                //handles potential exponent overflow 
+                if (exp_calc >= {EXP_WIDTH{1'b1}}) begin 
+                    float_result_s2 <= {sign_s1, {EXP_WIDTH{1'b1}}, {MANT_WIDTH{1'b0}}}; //infinity
+                end else begin 
+                    float_result_s2 = {sign_s1, exp_calc, mant_calc};
+                end
+            end
+            result_valid_s2 <= 1'b1;
+        end else begin 
+            result_valid_s2 <= 1'b0;
+        end
+    end
+
+    assign float_result = float_result_s2;
+    assign result_valid = result_valid_s2;
+endmodule
+
+`endif
